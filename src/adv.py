5c9cc881a3ed951a7bbec5aa94f79461d7ac38bc
unknown <tommyconner96@gmail.com>
2020-08-13 17:47:45 -0400

    items in rooms!

--- src/adv.py
+++ src/adv.py
@@ -2,36 +2,36 @@ from room import Room
 from player import Player
 from item import Item
 
-item = {
-    "knife", Item("knife", "A small knife")
-}
+# Declare all items
+item_knife = Item('knife', 'a small knife')
+item_lamp = Item('lamp', 'old brass lamp')
+item_watch = Item('watch', 'a pocket watch')
+item_lighter = Item('lighter', 'a cigarette lighter')
 
 # Declare all the rooms
-
 room = {
     'outside':  Room("Outside Cave Entrance",
                      "North of you, the cave mount beckons",
-                     ("knife", "a small knife")),
+                     item_knife),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
 passages run north and east.""",
-                    None),
+                    item_watch),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
 the distance, but there is no way across the chasm.""",
-                    None),
+                    item_lighter),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
 to north. The smell of gold permeates the air.""",
-                    None),
+                    item_lamp),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
 earlier adventurers. The only exit is to the south.""",
                     None),
 }
-
 
 
 # Link rooms together
@@ -66,7 +66,7 @@ while True:
     # * Prints the current description (the textwrap module might be useful here).
     print(f"{newPlayer.current_room.description}")
     if current_room.item is not None:
-        print(f"There is a {current_room.item[0]} in this room.\n")
+        print(f"There is a {current_room.item.name} in this room.\n")
     # * Waits for user input and decides what to do.
     move = input("Enter your next move or press Q to quit:\n")
     # If the user enters "q", quit the game.
